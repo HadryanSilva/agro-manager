d7a59af8849609a5f7c206e43943b92b21704936
Hadryan Silva <hadryan2001@gmail.com>
2026-04-16 16:23:54 -0300

    initial commit

---
+++ src/test/java/br/com/hadryan/agro/manager/AgroManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.hadryan.agro.manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
