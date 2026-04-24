dd145112ed16dddb97fccc3887314d0f55794900
Hadryan Silva <hadryan2001@gmail.com>
2026-04-24 17:42:07 -0300

    initial commit

---
+++ src/main/java/br/com/hadryan/agro/manager/AgroManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.hadryan.agro.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroManagerApplication.class, args);
+	}
+
+}
